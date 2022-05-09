22d527219ecafde532679c7fda64355c59017856
Sourav <sourav.iit.nstu@gmail.com>
2022-05-09 13:11:17 +0600

    innerHtml ExOne

---
+++ JavaScriptEvent/ExerciseOne.js
@@ -0,0 +1,8 @@
+// HTML Events: 
+// An HTML event can be something the browser does, or something a user does.
+
+// Here are some examples of HTML events:
+
+// An HTML web page has finished loading
+// An HTML input field was changed
+// An HTML button was clicked
